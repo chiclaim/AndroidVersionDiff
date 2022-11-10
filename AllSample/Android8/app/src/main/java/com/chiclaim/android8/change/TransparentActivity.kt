475e5ba11d93db2d731e6e1fe9f9886a265436c4
chiclaim <chiclaim@gmail.com>
2022-11-10 17:48:18 +0800

    [update] Android 8 透明主题测试案例

---
+++ AllSample/Android8/app/src/main/java/com/chiclaim/android8/change/TransparentActivity.kt
@@ -0,0 +1,15 @@
+package com.chiclaim.android8.change
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ *
+ * Created by kumu@2dfire.com on 2022/11/10.
+ */
+class TransparentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_transparent)
+    }
+}
